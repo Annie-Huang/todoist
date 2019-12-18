6423258be793ab8a4ecee68f1f864a39295d4bc9
Annie Huang <annie.hn@gmail.com>
2019-12-18 16:28:21 +1100

    Create the filrebase settings and copy it into the app.

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import 'firebase/firebase-firestore'
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBukcet: '',
-    messageSenderId: '',
-    apiId: '',
+    apiKey: "AIzaSyC9KP-3Iq-i9iBfn-Y7XVBNG3qPnOQWcTI",
+    authDomain: "todolist-7fcaf.firebaseapp.com",
+    databaseURL: "https://todolist-7fcaf.firebaseio.com",
+    projectId: "todolist-7fcaf",
+    storageBucket: "todolist-7fcaf.appspot.com",
+    messagingSenderId: "863176982186",
+    appId: "1:863176982186:web:6ab4a188c9dff773029ad3"
 });
 
 export {firebaseConfig as firebase};
