165b6045eed782649c0e3fb20872b9d9ae7d59f1
Annie Huang <annie.hn@gmail.com>
2019-12-18 17:43:46 +1100

    Create onSnapshot method for unsubscribe for hooks. And setTasks within the method.

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -29,5 +29,23 @@ export const useTasks = selectedProject => {
                     ? (unsubscribe = unsubscribe.where('date', '==', ''))
                     : unsubscribe;                                                                              // else
 
+        unsubscribe = unsubscribe.onSnapshot(snapshot => {
+            const newTasks = snapshot.docs.map(task => ({
+                id: task.id,
+                ...task.data(),
+            }));
+
+            setTasks(
+                selectedProject === 'NEXT_7'
+                    ? newTasks.filter(
+                    task =>
+                        moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7 &&
+                        task.archived !== true
+                    )
+                    : newTasks.filter(task => task.archived !== true)
+            );
+
+        })
+
     }, [selectedProject]);
 };
