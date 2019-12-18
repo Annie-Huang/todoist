fc4c51e74fad93db5614ba39de47225426a9ba36
Annie Huang <annie.hn@gmail.com>
2019-12-18 17:55:02 +1100

    unsubscribe for useEffect(). And return {tasks, archivedTasks}; for useTasks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -47,7 +47,11 @@ export const useTasks = selectedProject => {
             );
 
             setArchivedTasks(newTasks.filter(task => task.archived !== false));
-        })
+        });
+
+        return () => unsubscribe();
 
     }, [selectedProject]);
+
+    return {tasks, archivedTasks};
 };
