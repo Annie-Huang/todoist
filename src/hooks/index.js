b75394ac4b44975ff9f540b7b55f8543abf614ac
Annie Huang <annie.hn@gmail.com>
2019-12-18 17:46:59 +1100

    Create new state archivedTasks and add setArchivedTasks in onSnapshot in hooks as well.

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -5,6 +5,7 @@ import { collatedTasksExist } from '../helpers';
 
 export const useTasks = selectedProject => {
     const [tasks, setTasks] = useState([]);
+    const [archivedTasks, setArchivedTasks] = useState([]);
     // users, setUsers
     // cats, setCats
 
@@ -45,6 +46,7 @@ export const useTasks = selectedProject => {
                     : newTasks.filter(task => task.archived !== true)
             );
 
+            setArchivedTasks(newTasks.filter(task => task.archived !== false));
         })
 
     }, [selectedProject]);
