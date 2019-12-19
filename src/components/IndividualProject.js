3d16812ee8c4a82ac37f2ca6d40aa7979811fb62
Annie Huang <annie.hn@gmail.com>
2019-12-19 18:18:49 +1100

    Create the basic layout of IndividualProject.js

---
+++ src/components/IndividualProject.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import  {FaTrashAlt} from 'react-icons/fa';
+import {useProjectsValue, useSelectedProjectValue} from '../context';
+import {firebase} from "../firebase";
+
+export const IndividualPorject = ({project}) => {
+    const [showConfirm, setShowConfirm] = useState(false);
+    const { projects, setProjects } = useProjectsValue();
+    const { setSelectedProject } = useSelectedProjectValue();
+
+    const deleteProject = docId => {
+        firebase
+            .firestore()
+            .collection('projects')
+            .doc(docId)
+            .delete()
+            .then(() => {
+                setProjects([...projects]);
+                setSelectedProject('INBOX');
+            })
+    };
+
+    return (
+        <>
+            <span className="sidebar__dot">•</span>
+            <span className="sidebar__project-name">{project.name}</span>
+        </>
+    )
+
+};
